861e2b90f7b48d1a811954d7c9cdfff186f96738
Sally-N <snzungula@gmail.com>
2023-02-02 22:23:09 +0300

    object constructor

--- Script.js
+++ Script.js
@@ -11,11 +11,28 @@ function displayModal(){
 }
 
 function hideModal(){
-    bookAdditionForm.style.display = "none";
+    bookAdditionModal.style.display = "none";
 }
 
 additionButton.addEventListener("click", displayModal)
 additionButton.addEventListener("mousemove", displayModal);
 closeIcon.addEventListener("click", hideModal);
 closeIcon.addEventListener("mousemove", hideModal);
+bookAdditionForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+})
 
+////book constructor
+class Book{
+    constructor(title, author, pages, read){
+        this.title = form.title.value
+        this.author = form.author.value
+        this.pages = form.pages.value + 'pg';
+        this.read = form.read.checked;
+    }
+}
+
+
+////create book from constructorr and add to library
+let myLibrary = []
+let newBook 
